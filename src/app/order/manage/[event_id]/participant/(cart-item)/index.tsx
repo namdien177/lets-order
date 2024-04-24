a1aa80eadcfd3f38f635268f92071e4bfde35fc1
namdh3 <namdh3@smartosc.com>
2024-04-24 14:50:48 +0700

    update display ordered items in participant page

--- src/app/order/manage/[event_id]/participant/(cart-item)/index.tsx
+++ src/app/order/manage/[event_id]/participant/(cart-item)/index.tsx
@@ -1,6 +1,6 @@
 import { type UserCartInEvent } from "@/app/order/manage/[event_id]/participant/query";
 import { TableCell, TableRow } from "@/components/ui/table";
-import { formatAsMoney } from "@/lib/utils";
+import { cn, formatAsMoney } from "@/lib/utils";
 import { type Nullable } from "@/lib/types/helper";
 import { format } from "date-fns";
 import { Button } from "@/components/ui/button";
@@ -10,9 +10,10 @@ import {
 } from "@/server/db/constant";
 import { useMutation } from "@tanstack/react-query";
 import { markUserCartCompletePayment } from "./action";
-import { Loader } from "lucide-react";
+import { Loader, X } from "lucide-react";
 import { BaseResponseType } from "@/lib/types/response.type";
 import { toast } from "sonner";
+import { useState } from "react";
 
 type Props = {
   index: number;
@@ -39,6 +40,8 @@ const CartItem = ({ cart, index, onUpdated }: Props) => {
     }
   };
 
+  const [isViewingDetail, setIsViewingDetail] = useState(false);
+
   const { mutateAsync, isPending, data } = useMutation({
     mutationFn: markUserCartCompletePayment,
   });
@@ -55,11 +58,46 @@ const CartItem = ({ cart, index, onUpdated }: Props) => {
 
   return (
     <TableRow>
-      <TableCell>{index}</TableCell>
+      <TableCell className={"align-baseline"}>{index}</TableCell>
       <TableCell>
-        <div className="flex flex-col">
-          <p>{cart.clerkName}</p>
-          <small className={"text-muted-foreground"}>{cart.clerkEmail}</small>
+        <div className="flex flex-col gap-1">
+          <div className="flex flex-col">
+            <p>{cart.clerkName}</p>
+            <small className={"text-muted-foreground"}>{cart.clerkEmail}</small>
+          </div>
+          <div className="flex flex-col gap-1 pl-4 text-sm">
+            <div className={"flex items-center gap-1 text-muted-foreground"}>
+              <span>Ordered: {cart.item.length} item(s)</span>
+              <small
+                onClick={() => setIsViewingDetail((s) => !s)}
+                className={
+                  "cursor-pointer select-none rounded border px-2 py-1"
+                }
+              >
+                {isViewingDetail ? "Hide" : "View"} detail
+              </small>
+            </div>
+            <div
+              className={cn(
+                "flex flex-col gap-1 divide-y overflow-hidden rounded-lg border transition-[max-height] ease-in-out",
+                isViewingDetail ? "max-h-[999px]" : "max-h-0",
+              )}
+            >
+              {cart.item.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex flex-row items-center gap-1 p-2 text-sm"
+                >
+                  <p className={"flex-1"}>{item.name}</p>
+                  <p className={"text-muted-foreground"}>
+                    {formatAsMoney(item.price)}
+                  </p>
+                  <X size={16} />
+                  <p className={"text-muted-foreground"}>{item.amount}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </TableCell>
       <TableCell className={"text-right"}>{formatAsMoney(cartPrice)}</TableCell>
